44f15df9955a2adf08648bbd2751afb7eb1ada3e
anis5165 <anishkumargvm02@gmail.com>
2025-09-02 21:22:40 +0530

    functions and loops

---
+++ basics/iteration_tool.py
@@ -0,0 +1,26 @@
+# test = "anish"
+# if not test:
+#     print("code")
+
+# test = ""
+# if not test:
+#     print("chai")
+
+
+
+# myList = [1,2,3,4]
+
+# I = iter(myList)
+# print(I)
+
+# I.__next__()
+# print(I)
+# I.__next__()
+# print(I)
+# I.__next__()
+# print(I)
+
+
+
+
+
