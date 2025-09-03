9d65679958433adb3910242b4b4d534b4376101c
anis5165 <anishkumargvm02@gmail.com>
2025-09-03 16:49:08 +0530

    youtube video manager project

---
+++ basics/error_handle.py
@@ -0,0 +1,12 @@
+# file = open('files.txt','w')
+
+# try:
+#     file.write("video number - 21")
+# finally:
+#     file.close()
+
+
+    #another syntax
+
+with open("files.txt", 'w') as file:
+    file.write('chai wur cokdgjvewkdewdhe')
